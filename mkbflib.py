cdb1218d60671f12dd18cedcfe6b319a76661fd0
KotsuCoder <whoami@kotsu.red>
2026-04-20 08:42:12 -0700

    Initialize memory cells

--- mkbflib.py
+++ mkbflib.py
@@ -1,12 +1,10 @@
-import sys
-
 class BrainFProgram:
     def __init__(self, path):
         self.__instructions = self.__load_program(path)
         self.__instruction_pointer = 0
         self.__loop_set = None # Holds pointers to each loop beginning and end
-        self.__memory = None
-        self.__memory_pointer = None
+        self.__memory = [0 for i in range(0, 100000)]
+        self.__memory_pointer = 0
 
     def __load_program(self, path):
         with open(path, "r") as file:
@@ -17,14 +15,7 @@ class BrainFProgram:
             if char in valid_chars:
                 short_code.append(char)
         return short_code
-    
-    def print_program(self):
-        code_str = ""
-        for instruction in self.__instructions:
-            code_str += instruction
-        print(code_str)
 
 
 if __name__ == "__main__":
     helloworld = BrainFProgram("helloworld.bf")
-    helloworld.print_program()
