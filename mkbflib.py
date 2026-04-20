a5bfed36021a761115c58a1a93982bb85a41ef8c
KotsuCoder <whoami@kotsu.red>
2026-04-20 08:48:52 -0700

    Find the loop set for a BrainF program, class now fully initialized

--- mkbflib.py
+++ mkbflib.py
@@ -2,7 +2,7 @@ class BrainFProgram:
     def __init__(self, path):
         self.__instructions = self.__load_program(path)
         self.__instruction_pointer = 0
-        self.__loop_set = None # Holds pointers to each loop beginning and end
+        self.__loop_set = self.__get_loop_set()
         self.__memory = [0 for i in range(0, 100000)]
         self.__memory_pointer = 0
 
@@ -15,6 +15,19 @@ class BrainFProgram:
             if char in valid_chars:
                 short_code.append(char)
         return short_code
+    
+    def __get_loop_set(self):
+        loop_stack = []
+        loop_set = {}
+        for index, instruction in enumerate(self.__instructions):
+            if instruction == "[":
+                loop_stack.append(index)
+            if instruction == "]":
+                loop_start = loop_stack.pop()
+                loop_end = index
+                loop_set[loop_start] = loop_end
+                loop_set[loop_end] = loop_start
+        return loop_set
 
 
 if __name__ == "__main__":
