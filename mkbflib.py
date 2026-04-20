317aa6c5e33a633c891cdb0db771339d6d14aa05
KotsuCoder <whoami@kotsu.red>
2026-04-20 08:56:37 -0700

    Add documentation to code

--- mkbflib.py
+++ mkbflib.py
@@ -1,12 +1,18 @@
 class BrainFProgram:
-    def __init__(self, path):
+    def __init__(self, path: str):
+        """
+        An object representing a BrainF program with the ability to execute each instruction.
+        """
         self.__instructions = self.__load_program(path)
         self.__instruction_pointer = 0
         self.__loop_set = self.__get_loop_set()
         self.__memory = [0 for i in range(0, 100000)]
         self.__memory_pointer = 0
 
-    def __load_program(self, path):
+    def __load_program(self, path: str) -> list[str]:
+        """
+        Loads the BF program from the file at the given path.
+        """
         with open(path, "r") as file:
             full_code = file.read()
         valid_chars = ['+', '-', '.', ',', '[', ']']
@@ -16,7 +22,10 @@ class BrainFProgram:
                 short_code.append(char)
         return short_code
     
-    def __get_loop_set(self):
+    def __get_loop_set(self) -> dict[int, int]:
+        """
+        Grabs the indexes for the starts and ends of each loop and pairs them together in a dictionary.
+        """
         loop_stack = []
         loop_set = {}
         for index, instruction in enumerate(self.__instructions):
